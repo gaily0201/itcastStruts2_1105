ff54e3d247a52b4361b38e88dc6307e7965408a9
gaily <446029658@qq.com>
2013-10-20 14:26:17 +0800

    itcast1105_struts2

---
+++ src/cn/itcast/ognl/Person.java
@@ -0,0 +1,26 @@
+package cn.itcast.ognl;
+
+public class Person {
+
+	private String name = "pname";
+	
+	private String age = "30";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	
+}
